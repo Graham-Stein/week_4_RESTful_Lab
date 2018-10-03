03f65b52cdded784fe43dcc8be56dfbe6c4d9b4e
Graham-Stein <graham@guavoaviation.co.uk>
2018-10-03 14:58:24 +0100

    Add lab start point

---
+++ pizza_controller.rb
@@ -0,0 +1,31 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry-byebug' )
+require('pry')
+
+require_relative( './models/pizza_order' )
+also_reload( './models/*' )
+
+#index
+get('/pizza-orders') do
+  @pizzas = PizzaOrder.all()
+  erb(:index)
+end
+
+#NEW
+get ('/pizza-orders/new') do
+  erb(:new)
+end
+#SHOW
+get('/pizza-orders/:id') do
+  id = params[:id].to_i
+  @pizza = PizzaOrder.find(id)
+  erb(:show)
+end
+
+#CREATE
+post('/pizza-orders') do
+  @pizza = PizzaOrder.new(params)
+  @pizza.save()
+  erb(:create)
+end
